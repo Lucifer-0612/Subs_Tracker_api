61c0bfabba11a27b5f0b773c31a9f83c8fb16bb5
Lucifer-0612 <Ashuofficial1206@gmail.com>
2025-11-02 19:54:02 +0530

    added some comments

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -1,6 +1,7 @@
 import { Router } from "express";
 import { signin, signout, signup } from "../controllers/auth.controllers.js";
 const authRouter = Router();
+// Signup route
 
 authRouter.post('/signup', signup);
 
